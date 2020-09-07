ee7c06a63c57b09e68b192ebf15fa4a897f9c7c6
David Snyder <davidgraygs4@gmail.com>
2020-09-07 12:37:18 -0400

    update test file

---
+++ test/index.test.js
@@ -0,0 +1,32 @@
+/* global describe, it */
+
+const TwitchLib = require("../dist/index.ts");
+
+const clientId = process.env.ClientId;
+const authorizationKey = process.env.authorizationKey;
+if (!clientId || !authorizationKey) {
+	throw new Error("Missing client id or authorization key");
+}
+
+const testApi = new TwitchLib({
+    clientId,
+    authorizationKey
+});
+
+
+test("should have the right client id", () => {
+	expect(testApi.clientId).toBeDefined();
+});
+it("shouldn't be able to get moderators with given token", async function () {
+	const result = await testApi.getUserInfo("514845764");
+	expect(result).toBeDefined();
+});
+it("should get moderators properly", async function () {
+	const result = await testApi.getUserModerationChannels("dav1dsnyder404");
+	expect(result).toBeDefined();
+});
+it("should get mod channels properly", async function () {
+	const result = await testApi.fetchModChannels("alca");
+	expect(result).toBeDefined();
+	// expect(result.length).equals(116)
+});
